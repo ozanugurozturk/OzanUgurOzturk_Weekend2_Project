282d7325acc3448c33eec415476f13756b607fcd
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-11-30 15:27:34 +0100

    Edit 2

--- script.js
+++ script.js
@@ -1,243 +1,215 @@
-/*Here we have created two different arrays that you can work with if you want.
-If you choose to create your own arrays with elements, just make sure that some
-of the properties make sense to filter on, and some to sort on.*/
-
 const books = [
   {
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
     year: 1925,
-    genre: 'Fiction',
+    genre: "Fiction",
     rating: 4.2,
     description:
-      'A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.',
-    image: './books-images/the-great-gatsby.jpg'
+      "A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.",
+    image: "./books-images/the-great-gatsby.jpg",
   },
   {
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee',
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
     year: 1960,
-    genre: 'Fiction',
+    genre: "Fiction",
     rating: 4.5,
     description:
-      'Set in the American South, this novel tackles issues of racism and injustice through the eyes of young Scout Finch.',
-    image: './books-images/to-kill-a-mockingbird.jpg'
+      "Set in the American South, this novel tackles issues of racism and injustice through the eyes of young Scout Finch.",
+    image: "./books-images/to-kill-a-mockingbird.jpg",
   },
   {
-    title: '1984',
-    author: 'George Orwell',
+    title: "1984",
+    author: "George Orwell",
     year: 1949,
-    genre: 'Science Fiction',
+    genre: "Science Fiction",
     rating: 4.4,
     description:
-      'A dystopian classic that explores totalitarianism and the consequences of a surveillance state in a bleak future.',
-    image: './books-images/1984.jpg'
+      "A dystopian classic that explores totalitarianism and the consequences of a surveillance state in a bleak future.",
+    image: "./books-images/1984.jpg",
   },
   {
-    title: 'Pride and Prejudice',
-    author: 'Jane Austen',
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
     year: 1813,
-    genre: 'Fiction',
+    genre: "Fiction",
     rating: 4.25,
     description:
-      'A timeless romance novel that examines societal expectations and the misunderstandings that can arise from pride and prejudice.',
-    image: './books-images/pride-and-prejudice.jpg'
+      "A timeless romance novel that examines societal expectations and the misunderstandings that can arise from pride and prejudice.",
+    image: "./books-images/pride-and-prejudice.jpg",
   },
   {
-    title: 'The Catcher in the Rye',
-    author: 'J.D. Salinger',
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
     year: 1951,
-    genre: 'Fiction',
+    genre: "Fiction",
     rating: 4,
     description:
-      'Narrated by the teenage Holden Caulfield, the novel explores themes of alienation and the search for authenticity.',
-    image: './books-images/unknown.jpg'
+      "Narrated by the teenage Holden Caulfield, the novel explores themes of alienation and the search for authenticity.",
+    image: "./books-images/unknown.jpg",
   },
   {
-    title: 'The Hobbit',
-    author: 'J.R.R. Tolkien',
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
     year: 1937,
-    genre: 'Fantasy',
+    genre: "Fantasy",
     rating: 4.6,
     description:
-      'A fantasy adventure novel that follows Bilbo Baggins on a quest to help a group of dwarves reclaim their homeland from a dragon.',
-    image: './books-images/the-hobbit.jpg'
+      "A fantasy adventure novel that follows Bilbo Baggins on a quest to help a group of dwarves reclaim their homeland from a dragon.",
+    image: "./books-images/the-hobbit.jpg",
   },
   {
     title: "Harry Potter and the Sorcerer's Stone",
-    author: 'J.K. Rowling',
+    author: "J.K. Rowling",
     year: 1997,
-    genre: 'Fantasy',
+    genre: "Fantasy",
     rating: 4.7,
     description:
-      'The first book in the beloved Harry Potter series, it introduces readers to the magical world of Hogwarts and the young wizard Harry Potter.',
-    image: "./books-images/harry-potter-and-the-sorcerer.jpg"
+      "The first book in the beloved Harry Potter series, it introduces readers to the magical world of Hogwarts and the young wizard Harry Potter.",
+    image: "./books-images/harry-potter-and-the-sorcerer.jpg",
   },
   {
-    title: 'Moby-Dick',
-    author: 'Herman Melville',
+    title: "Moby-Dick",
+    author: "Herman Melville",
     year: 1851,
-    genre: 'Adventure',
+    genre: "Adventure",
     rating: 4.1,
     description:
-      'An epic tale of obsession, revenge, and the relentless pursuit of the great white whale, Moby Dick.',
-    image: './books-images/moby-dick.jpg'
+      "An epic tale of obsession, revenge, and the relentless pursuit of the great white whale, Moby Dick.",
+    image: "./books-images/moby-dick.jpg",
   },
   {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    author: 'J.R.R. Tolkien',
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    author: "J.R.R. Tolkien",
     year: 1954,
-    genre: 'Fantasy',
+    genre: "Fantasy",
     rating: 4.55,
     description:
-      'The first volume of the epic fantasy trilogy follows Frodo Baggins and the Fellowship on their quest to destroy the One Ring.',
-    image: './books-images/the-lord-of-the-rings.jpg'
+      "The first volume of the epic fantasy trilogy follows Frodo Baggins and the Fellowship on their quest to destroy the One Ring.",
+    image: "./books-images/the-lord-of-the-rings.jpg",
   },
   {
-    title: 'The Shining',
-    author: 'Stephen King',
+    title: "The Shining",
+    author: "Stephen King",
     year: 1977,
-    genre: 'Horror',
+    genre: "Horror",
     rating: 4.3,
     description:
       "A psychological horror novel that tells the story of the Torrance family's terrifying experiences at the haunted Overlook Hotel.",
-    image: './books-images/unknown.jpg'
+    image: "./books-images/unknown.jpg",
   },
   {
-    title: 'The Chronicles of Narnia: The Lion, the Witch and the Wardrobe',
-    author: 'C.S. Lewis',
+    title: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+    author: "C.S. Lewis",
     year: 1950,
-    genre: 'Fantasy',
+    genre: "Fantasy",
     rating: 4.15,
     description:
-      'The first book in the Chronicles of Narnia series, it follows the adventures of children who discover the magical land of Narnia.',
-    image: './books-images/the-chronicles-of-narnia.jpg'
+      "The first book in the Chronicles of Narnia series, it follows the adventures of children who discover the magical land of Narnia.",
+    image: "./books-images/the-chronicles-of-narnia.jpg",
   },
   {
-    title: 'The Da Vinci Code',
-    author: 'Dan Brown',
+    title: "The Da Vinci Code",
+    author: "Dan Brown",
     year: 2003,
-    genre: 'Mystery',
+    genre: "Mystery",
     rating: 3.8,
     description:
-      'A gripping mystery thriller that follows Harvard symbologist Robert Langdon as he unravels the secrets of the Da Vinci Code.',
-    image: './books-images/unknown.jpg'
+      "A gripping mystery thriller that follows Harvard symbologist Robert Langdon as he unravels the secrets of the Da Vinci Code.",
+    image: "./books-images/unknown.jpg",
   },
   {
-    title: 'The Alchemist',
-    author: 'Paulo Coelho',
+    title: "The Alchemist",
+    author: "Paulo Coelho",
     year: 1988,
-    genre: 'Fiction',
+    genre: "Fiction",
     rating: 4.25,
     description:
-      'A philosophical novel that tells the story of Santiago, a shepherd boy, on his quest to discover his personal legend.',
-    image: './books-images/unknown.jpg'
+      "A philosophical novel that tells the story of Santiago, a shepherd boy, on his quest to discover his personal legend.",
+    image: "./books-images/unknown.jpg",
   },
   {
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
+    title: "The Hunger Games",
+    author: "Suzanne Collins",
     year: 2008,
-    genre: 'Science Fiction',
+    genre: "Science Fiction",
     rating: 4.3,
     description:
       "In a dystopian future, Katniss Everdeen becomes a symbol of rebellion when she volunteers to take her sister's place in the brutal Hunger Games.",
-    image: './books-images/unknown.jpg'
+    image: "./books-images/unknown.jpg",
   },
   {
-    title: 'The Girl with the Dragon Tattoo',
-    author: 'Stieg Larsson',
+    title: "The Girl with the Dragon Tattoo",
+    author: "Stieg Larsson",
     year: 2005,
-    genre: 'Mystery',
+    genre: "Mystery",
     rating: 4.1,
     description:
-      'A gripping mystery novel featuring investigative journalist Mikael Blomkvist and the enigmatic hacker Lisbeth Salander.',
-    image: './books-images/unknown.jpg'
+      "A gripping mystery novel featuring investigative journalist Mikael Blomkvist and the enigmatic hacker Lisbeth Salander.",
+    image: "./books-images/unknown.jpg",
   },
   {
-    title: 'The Road',
-    author: 'Cormac McCarthy',
+    title: "The Road",
+    author: "Cormac McCarthy",
     year: 2006,
-    genre: 'Dystopian',
+    genre: "Dystopian",
     rating: 4,
     description:
       "Set in a post-apocalyptic world, it follows a father and son's harrowing journey to survive and find safety.",
-    image: './books-images/unknown.jpg'
+    image: "./books-images/unknown.jpg",
   },
   {
     title: "The Hitchhiker's Guide to the Galaxy",
-    author: 'Douglas Adams',
+    author: "Douglas Adams",
     year: 1979,
-    genre: 'Science Fiction',
+    genre: "Science Fiction",
     rating: 4.35,
     description:
       "A comedic science fiction series that follows the misadventures of Arthur Dent after Earth's destruction.",
-    image: './books-images/unknown.jpg'
+    image: "./books-images/unknown.jpg",
   },
   {
-    title: 'The Giver',
-    author: 'Lois Lowry',
+    title: "The Giver",
+    author: "Lois Lowry",
     year: 1993,
-    genre: 'Dystopian',
+    genre: "Dystopian",
     rating: 4.12,
     description:
-      'A dystopian novel set in a seemingly perfect society where young Jonas discovers the dark truth beneath the surface.',
-    image: './books-images/unknown.jpg'
-  }
-]
+      "A dystopian novel set in a seemingly perfect society where young Jonas discovers the dark truth beneath the surface.",
+    image: "./books-images/unknown.jpg",
+  },
+];
 
 document.addEventListener("DOMContentLoaded", function () {
-  let allItems = books;
+  const allItems = books;
   let currentItems = allItems;
 
-  // Displaying images and names
   function displayItems(items, container, cardClass) {
-      container.innerHTML = ""; // Container cleaner
+      container.innerHTML = "";
       items.forEach((item) => {
           const card = document.createElement("div");
           card.classList.add("card", cardClass);
 
-          // Creating image element
           const itemImage = document.createElement("img");
           itemImage.src = item.image;
           itemImage.alt = item.title;
 
-          // Creating headers for the objects
           const itemName = document.createElement("h2");
           itemName.textContent = item.title;
 
-          // Appending the image and the name to the card
           card.appendChild(itemImage);
           card.appendChild(itemName);
 
-          // Putting the card in the container object
           container.appendChild(card);
       });
   }
 
-  // Showing all items
-  function showAllItems() {
-      currentItems = allItems;
-      displayItems(currentItems, bookListContainer, "book-card");
-      document.getElementById("book-list").style.display = "block";
-  }
-
-  // Referencing containers
   const bookListContainer = document.getElementById("book-list-container");
-
-  // Referencing buttons
-  const showAllButton = document.getElementById("showAll");
-
-  // Adding Event Listener
-  showAllButton.addEventListener("click", showAllItems);
-
-  // Get the filterGenre element
   const filterGenreSelect = document.getElementById("filterGenre");
 
-  // Populate the filterGenre select options
   function populateFilters() {
-      const allGenres = getAllGenres(); // Get all unique genres from books
-
-      // Populate the filterGenre select with unique genres from books
+      const allGenres = getAllGenres();
       filterGenreSelect.innerHTML = '<option value="">All</option>';
       allGenres.forEach((genre) => {
           const option = document.createElement("option");
@@ -247,43 +219,40 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  // Get all unique genres from books
   function getAllGenres() {
       const genres = new Set();
       books.forEach((book) => genres.add(book.genre));
       return Array.from(genres);
   }
 
-  // Filter books by genre
   function filterBooksByGenre(genre) {
       if (genre === "") {
-          showAllItems();
+          displayItems(allItems, bookListContainer, "book-card");
       } else {
           const filteredBooks = books.filter((book) => book.genre === genre);
           displayItems(filteredBooks, bookListContainer, "book-card");
       }
   }
 
-  // Adding event listeners for filterGenre select element
+  function showAllItems() {
+      displayItems(allItems, bookListContainer, "book-card");
+  }
+
   filterGenreSelect.addEventListener("change", function () {
       const selectedGenre = filterGenreSelect.value;
       filterBooksByGenre(selectedGenre);
   });
 
-  // Referencing select elements for sorting
   const sortPropertySelect = document.getElementById("sortProperty");
   const sortOrderSelect = document.getElementById("sortOrder");
 
-  // Adding event listeners for sorting
   sortPropertySelect.addEventListener("change", sortItems);
   sortOrderSelect.addEventListener("change", sortItems);
 
-  // Function to sort items
   function sortItems() {
       const selectedProperty = sortPropertySelect.value;
       const selectedOrder = sortOrderSelect.value;
 
-      // Sort the currentItems array based on the selected property and order
       if (selectedProperty && selectedOrder) {
           currentItems.sort((a, b) => {
               if (selectedOrder === "asc") {
@@ -294,13 +263,9 @@ document.addEventListener("DOMContentLoaded", function () {
           });
       }
 
-      // Redisplaying the sorted items
       displayItems(currentItems, bookListContainer, "book-card");
   }
 
-  // Calling the function to populate the filters initially
   populateFilters();
-
-  // Default display is showing all items
-  showAllItems();
+  showAllItems(); // Display all items by default
 });
