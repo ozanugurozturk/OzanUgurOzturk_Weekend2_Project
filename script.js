e5583a367fccd4cb5890edc2109ce1fbf0dde30c
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-11-30 14:57:53 +0100

    Edit 1

--- script.js
+++ script.js
@@ -185,380 +185,89 @@ const books = [
   }
 ]
 
-const recipes = [
-  {
-    name: 'Individual vegetarian lasagnes',
-    cuisineType: ['italian'],
-    ingredients: [
-      '1.2 kg cherry tomatoes',
-      '5 sprigs of fresh thyme',
-      'extra virgin olive oil',
-      '2 shallots',
-      '2 cloves of garlic',
-      '500 g baby spinach',
-      '8-12 fresh or dried lasagne sheets',
-      '350 g ricotta cheese',
-      'WHITE SAUCE',
-      '600 ml milk',
-      '25 g unsalted butter',
-      '2 heaped tablespoons flour',
-      '150 g vegetarian sharp, mature cheese',
-      '100 g mozzarella'
-    ],
-    source: 'Jamie Oliver',
-    totalTime: 130,
-    url: 'http://www.jamieoliver.com/recipes/vegetables-recipes/individual-vegetarian-lasagnes/',
-    image: './recipe-images/individual-vegetarian-lasagnes.jpg'
-  },
-  {
-    name: 'Vegetarian Stir-Fried Garlic Scape',
-    cuisineType: ['Balanced'],
-    ingredients: [
-      '8 oz. garlic scapes',
-      '3 oz. baby corn',
-      '3 oz. carrots',
-      '1 oz. dried shiitake mushrooms',
-      '1 clove of garlic sliced thinly',
-      '3 slices of fresh ginger root',
-      '2 tablespoons vegetable oil',
-      '1/4 cup shaoxing cooking wine',
-      '1/4 vegetarian stock or water',
-      '1 tablespoon light soy sauce',
-      '1 teaspoon sugar',
-      '1 teaspoon cornstarch',
-      '1/4 teaspoon ground white pepper'
-    ],
-    source: 'Red Cook',
-    totalTime: null,
-    URL: 'http://redcook.net/2010/06/16/garlic-scape-an-off-menu-treat/',
-    image: './recipe-images/vegetarian-stir-fried-garlic-s.jpg'
-  },
-  {
-    name: 'Cheat’s cheesy focaccia',
-    cuisineType: ['Italian'],
-    ingredients: [
-      '500g pack bread mix',
-      '2 tbsp olive oil , plus a little extra for drizzling',
-      '25g parmesan (or vegetarian alternative), grated',
-      '75g dolcelatte cheese (or vegetarian alternative)'
-    ],
-    source: 'BBC Good Food',
-    totalTime: 40,
-    URL: 'https://www.bbcgoodfood.com/recipes/cheats-cheesy-focaccia',
-    image: './recipe-images/cheat’s-cheesy-focaccia.jpg'
-  },
-  {
-    name: "Vegetarian Shepherd's Pie",
-    cuisineType: ['Balanced', 'High-Fiber'],
-    ingredients: [
-      '2 tablespoons extra-virgin olive oil',
-      '1 large onion, finely diced',
-      '2 carrots, peeled and thinly sliced',
-      '2 celery stalks, thinly sliced',
-      '10 ounces cremini mushrooms, trimmed and sliced',
-      '1 tablespoon tomato paste',
-      "1 tablespoon vegetarian Worcestershire sauce, such as Annie's Naturals",
-      '1 dried bay leaf',
-      '1 cup French green lentils, picked over',
-      'Kosher salt and freshly ground pepper',
-      '1 cup frozen peas',
-      '2 pounds Yukon Gold potatoes, scrubbed and cut into 1 1/2-inch pieces',
-      '4 cloves garlic',
-      '4 tablespoons unsalted butter',
-      '1/2 cup whole milk, warmed'
-    ],
-    source: 'Martha Stewart',
-    totalTime: 120,
-    URL: 'https://www.marthastewart.com/1535235/vegetarian-shepherds-pie',
-    image: "./recipe-images/vegetarian-shepherd's-pie.jpg"
-  },
-  {
-    name: 'Chicken Paprikash',
-    cuisineType: ['Low-Carb'],
-    ingredients: [
-      '640 grams chicken - drumsticks and thighs ( 3 whole chicken legs cut apart)',
-      '1/2 teaspoon salt',
-      '1/4 teaspoon black pepper',
-      '1 tablespoon butter – cultured unsalted (or olive oil)',
-      '240 grams onion sliced thin (1 large onion)',
-      '70 grams Anaheim pepper chopped (1 large pepper)',
-      '25 grams paprika (about 1/4 cup)',
-      '1 cup chicken stock',
-      '1/2 teaspoon salt',
-      '1/2 cup sour cream',
-      '1 tablespoon flour – all-purpose'
-    ],
-    source: 'No Recipes',
-    totalTime: 80,
-    URL: 'http://norecipes.com/recipe/chicken-paprikash/',
-    image: './recipe-images/chicken-paprikash.jpg'
-  },
-  {
-    name: 'Baked Chicken',
-    cuisineType: ['american'],
-    ingredients: [
-      '6 bone-in chicken breast halves, or 6 chicken thighs and wings, skin-on',
-      '1/2 teaspoon coarse salt',
-      '1/2 teaspoon Mrs. Dash seasoning',
-      '1/4 teaspoon freshly ground black pepper'
-    ],
-    source: 'Martha Stewart',
-    totalTime: 90,
-    url: 'http://www.marthastewart.com/318981/baked-chicken',
-    image: './recipe-images/baked-chicken.jpg'
-  },
-  {
-    name: 'Deep Fried Fish Bones',
-    cuisineType: ['south east asian'],
-    ingredients: ['8 small whiting fish or smelt', '4 cups vegetable oil'],
-    source: 'Serious Eats',
-    totalTime: 31,
-    url: 'http://www.seriouseats.com/recipes/2011/03/deep-fried-fish-bones-recipe.html',
-    image: './recipe-images/deep-fried-fish-bones.jpg'
-  },
-  {
-    name: 'Burnt-Scallion Fish',
-    cuisineType: ['chinese'],
-    ingredients: [
-      '2 bunches scallions',
-      '8 tbsp. butter',
-      '2 8-oz. fish filets'
-    ],
-    source: 'Saveur',
-    totalTime: 70,
-    url: 'http://www.saveur.com/article/Recipes/Burnt-Scallion-Fish',
-    image: './recipe-images/fish-dish.jpg'
-  },
-  {
-    name: 'Curry-Crusted Fish',
-    cuisineType: ['south east asian'],
-    ingredients: [
-      '3 slices bread , about 85g/3oz in total',
-      '1 lime',
-      '1.0 tbsp Korma curry paste',
-      '4 thick white fish fillets'
-    ],
-    source: 'BBC Good Food',
-    totalTime: 80,
-    url: 'http://www.bbcgoodfood.com/recipes/4717/',
-    image: './recipe-images/fish-dish.jpg'
-  },
-  {
-    name: 'Meat Stock',
-    cuisineType: 'american',
-    ingredients: [
-      '2.5 pounds beef marrow bones',
-      '1 large onion, quartered',
-      '2 carrots, sliced',
-      '1 leek, cleaned and sliced',
-      '2 celery stalks, sliced',
-      '2.5 pounds organic beef stew meat, cubed',
-      '2 tablespoons tomato paste',
-      '5 cloves garlic',
-      '2 bay leaves',
-      '3 sprigs thyme',
-      '3 sprigs Italian parsley',
-      '1/2 teaspoon black peppercorns'
-    ],
-    source: 'Food52',
-    totalTime: 60,
-    url: 'https://food52.com/recipes/3712-meat-stock',
-    image: './recipe-images/meat.jpg'
-  },
-  {
-    name: 'Homemade Meat Broth',
-    cuisineType: 'american',
-    ingredients: [
-      '1 teaspoon salt',
-      '1 carrot, peeled',
-      '1 medium onion, peeled',
-      '2 or 3 celery stalks',
-      '¼ red or yellow bell pepper, stripped of all its seeds',
-      '1 small boiling potato, peeled',
-      '1 ripe, fresh tomato, or 1 canned Italian plum tomato, drained of juice',
-      '5 pounds assorted pieces of meat and bones (see meat suggestions above), of which no less than 1½ pounds is all meat'
-    ],
-    source: 'Cookstr',
-    totalTime: 60,
-    url: 'http://www.cookstr.com/recipes/il-brodo-homemade-meat-broth',
-    image: './recipe-images/meat.jpg'
-  },
-  {
-    name: 'Spice-Rubbed Grilled Flap Meat (Sirloin Tip) Recipe',
-    cuisineType: 'south-american',
-    ingredients: [
-      '1 tablespoon whole black peppercorns, toasted',
-      '1 teaspoon coriander seed, toasted',
-      '1 teaspoon fennel seed, toasted',
-      '1 teaspoon cumin pods, toasted',
-      '1 teaspoon red pepper flakes',
-      '1/2 teaspoon dried oregano',
-      '2 medium cloves garlic, minced (about 2 teaspoons)',
-      '2 tablespoons vegetable or canola oil',
-      '1 whole flap meat steak, 2 to 2 1/2 pounds',
-      'Kosher salt'
-    ],
-    source: 'Serious Eats',
-    totalTime: 240,
-    url: 'http://www.seriouseats.com/recipes/2012/05/spice-rubbed-grilled-flap-meat-sirloin-tip-recipe.html',
-    image: './recipe-images/grilled.jpg'
-  }
-]
-
 document.addEventListener("DOMContentLoaded", function () {
-  let allItems = books.concat(recipes);
+  let allItems = books;
   let currentItems = allItems;
 
   // Displaying images and names
   function displayItems(items, container, cardClass) {
-    container.innerHTML = ""; // Container cleaner
-    items.forEach((item) => {
-      const card = document.createElement("div");
-      card.classList.add("card", cardClass);
+      container.innerHTML = ""; // Container cleaner
+      items.forEach((item) => {
+          const card = document.createElement("div");
+          card.classList.add("card", cardClass);
 
-      // Creating image element
-      const itemImage = document.createElement("img");
-      itemImage.src = item.image;
-      itemImage.alt = item.title || item.name;
+          // Creating image element
+          const itemImage = document.createElement("img");
+          itemImage.src = item.image;
+          itemImage.alt = item.title;
 
-      // Creating headers for the objects
-      const itemName = document.createElement("h2");
-      itemName.textContent = item.title || item.name;
+          // Creating headers for the objects
+          const itemName = document.createElement("h2");
+          itemName.textContent = item.title;
 
-      // Appending the image and the name to the card
-      card.appendChild(itemImage);
-      card.appendChild(itemName);
+          // Appending the image and the name to the card
+          card.appendChild(itemImage);
+          card.appendChild(itemName);
 
-      // Putting the card in the container object
-      container.appendChild(card);
-    });
+          // Putting the card in the container object
+          container.appendChild(card);
+      });
   }
 
   // Showing all items
   function showAllItems() {
-    currentItems = allItems;
-    displayItems(books, bookListContainer, "book-card");
-    displayItems(recipes, recipeListContainer, "recipe-card");
-    document.getElementById("book-list").style.display = "block";
-    document.getElementById("recipe-list").style.display = "block";
-  }
-
-  // Showing only books
-  function showBooks() {
-    currentItems = books;
-    displayItems(currentItems, bookListContainer, "book-card");
-    recipeListContainer.innerHTML = ""; // Clearing the recipe container
-    document.getElementById("book-list").style.display = "block";
-    document.getElementById("recipe-list").style.display = "none";
-  }
-
-  // Showing only recipes
-  function showRecipes() {
-    currentItems = recipes;
-    displayItems(currentItems, recipeListContainer, "recipe-card");
-    bookListContainer.innerHTML = ""; // Clearing the book container
-    document.getElementById("book-list").style.display = "none";
-    document.getElementById("recipe-list").style.display = "block";
+      currentItems = allItems;
+      displayItems(currentItems, bookListContainer, "book-card");
+      document.getElementById("book-list").style.display = "block";
   }
 
   // Referencing containers
   const bookListContainer = document.getElementById("book-list-container");
-  const recipeListContainer = document.getElementById("recipe-list-container");
 
   // Referencing buttons
   const showAllButton = document.getElementById("showAll");
-  const showBooksButton = document.getElementById("showBooks");
-  const showRecipesButton = document.getElementById("showRecipes");
 
   // Adding Event Listener
-
   showAllButton.addEventListener("click", showAllItems);
-  showBooksButton.addEventListener("click", showBooks);
-  showRecipesButton.addEventListener("click", showRecipes);
 
-  // Get the filterGenre and filterCuisine elements
+  // Get the filterGenre element
   const filterGenreSelect = document.getElementById("filterGenre");
-  const filterCuisineSelect = document.getElementById("filterCuisine");
 
-  // Populate the filterGenre and filterCuisine select options
+  // Populate the filterGenre select options
   function populateFilters() {
-    const allGenres = getAllGenres(); // Get all unique genres from books
-    const allCuisineTypes = getAllCuisineTypes(); // Get all unique cuisineTypes from recipes
+      const allGenres = getAllGenres(); // Get all unique genres from books
 
-    // Populate the filterGenre select with unique genres from books
-    filterGenreSelect.innerHTML = '<option value="">All</option>';
-    allGenres.forEach((genre) => {
-      const option = document.createElement("option");
-      option.value = genre;
-      option.textContent = genre;
-      filterGenreSelect.appendChild(option);
-    });
-
-    // Populate the filterCuisine select with unique cuisineTypes from recipes
-    filterCuisineSelect.innerHTML = '<option value="">All</option>';
-    allCuisineTypes.forEach((cuisineType) => {
-      const option = document.createElement("option");
-      option.value = cuisineType;
-      option.textContent = cuisineType;
-      filterCuisineSelect.appendChild(option);
-    });
+      // Populate the filterGenre select with unique genres from books
+      filterGenreSelect.innerHTML = '<option value="">All</option>';
+      allGenres.forEach((genre) => {
+          const option = document.createElement("option");
+          option.value = genre;
+          option.textContent = genre;
+          filterGenreSelect.appendChild(option);
+      });
   }
 
   // Get all unique genres from books
   function getAllGenres() {
-    const genres = new Set();
-    books.forEach((book) => genres.add(book.genre));
-    return Array.from(genres);
-  }
-
-  // Get all unique cuisineTypes from recipes
-  function getAllCuisineTypes() {
-    const cuisineTypes = new Set();
-    recipes.forEach((recipe) => {
-      if (Array.isArray(recipe.cuisineType)) {
-        recipe.cuisineType.forEach((cuisine) => cuisineTypes.add(cuisine));
-      } else if (typeof recipe.cuisineType === "string") {
-        cuisineTypes.add(recipe.cuisineType);
-      }
-    });
-    return Array.from(cuisineTypes);
+      const genres = new Set();
+      books.forEach((book) => genres.add(book.genre));
+      return Array.from(genres);
   }
 
   // Filter books by genre
   function filterBooksByGenre(genre) {
-    if (genre === "") {
-      showAllItems();
-    } else {
-      const filteredBooks = books.filter((book) => book.genre === genre);
-      displayItems(filteredBooks, bookListContainer, "book-card");
-      recipeListContainer.innerHTML = ""; // Clear the recipe container
-    }
+      if (genre === "") {
+          showAllItems();
+      } else {
+          const filteredBooks = books.filter((book) => book.genre === genre);
+          displayItems(filteredBooks, bookListContainer, "book-card");
+      }
   }
 
-  // Filtering recipes by cuisineType
-  function filterRecipesByCuisineType(cuisineType) {
-    if (cuisineType === "") {
-      showAllItems();
-    } else {
-      const filteredRecipes = recipes.filter((recipe) =>
-        Array.isArray(recipe.cuisineType)
-          ? recipe.cuisineType.includes(cuisineType)
-          : recipe.cuisineType === cuisineType
-      );
-      displayItems(filteredRecipes, recipeListContainer, "recipe-card");
-      bookListContainer.innerHTML = ""; // Clear the book container
-    }
-  }
-
-  // Adding event listeners for filterGenre and filterCuisine select elements
+  // Adding event listeners for filterGenre select element
   filterGenreSelect.addEventListener("change", function () {
-    const selectedGenre = filterGenreSelect.value;
-    filterBooksByGenre(selectedGenre);
-  });
-
-  filterCuisineSelect.addEventListener("change", function () {
-    const selectedCuisineType = filterCuisineSelect.value;
-    filterRecipesByCuisineType(selectedCuisineType);
+      const selectedGenre = filterGenreSelect.value;
+      filterBooksByGenre(selectedGenre);
   });
 
   // Referencing select elements for sorting
@@ -571,26 +280,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Function to sort items
   function sortItems() {
-    const selectedProperty = sortPropertySelect.value;
-    const selectedOrder = sortOrderSelect.value;
+      const selectedProperty = sortPropertySelect.value;
+      const selectedOrder = sortOrderSelect.value;
 
-    // Sort the currentItems array based on the selected property and order
-    if (selectedProperty && selectedOrder) {
-      currentItems.sort((a, b) => {
-        if (selectedOrder === "asc") {
-          return a[selectedProperty] > b[selectedProperty] ? 1 : -1;
-        } else {
-          return b[selectedProperty] > a[selectedProperty] ? 1 : -1;
-        }
-      });
-    }
+      // Sort the currentItems array based on the selected property and order
+      if (selectedProperty && selectedOrder) {
+          currentItems.sort((a, b) => {
+              if (selectedOrder === "asc") {
+                  return a[selectedProperty] > b[selectedProperty] ? 1 : -1;
+              } else {
+                  return b[selectedProperty] > a[selectedProperty] ? 1 : -1;
+              }
+          });
+      }
 
-    // Redisplaying the sorted items
-    if (currentItems === books) {
+      // Redisplaying the sorted items
       displayItems(currentItems, bookListContainer, "book-card");
-    } else {
-      displayItems(currentItems, recipeListContainer, "recipe-card");
-    }
   }
 
   // Calling the function to populate the filters initially
